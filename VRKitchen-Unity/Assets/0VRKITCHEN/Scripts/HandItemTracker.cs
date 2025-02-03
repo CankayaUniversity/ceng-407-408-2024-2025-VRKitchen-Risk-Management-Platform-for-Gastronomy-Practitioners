bac0a7e6abc8e409826b295d52f8684d13400234
Tuna Yavuz <tunayavuz.95@hotmail.com>
2025-02-03 16:47:10 +0300

    item control over tags added

---
+++ VRKitchen-Unity/Assets/0VRKITCHEN/Scripts/HandItemTracker.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandItemTracker : MonoBehaviour
+{
+    private XRDirectInteractor interactor;
+    public List<string> heldItemTags = new List<string>();
+
+    private void Awake()
+    {
+        interactor = GetComponent<XRDirectInteractor>();
+    }
+
+    private void OnEnable()
+    {
+        interactor.selectEntered.AddListener(OnItemGrabbed);
+        interactor.selectExited.AddListener(OnItemReleased);
+    }
+
+    private void OnDisable()
+    {
+        interactor.selectEntered.RemoveListener(OnItemGrabbed);
+        interactor.selectExited.RemoveListener(OnItemReleased);
+    }
+
+    private void OnItemGrabbed(SelectEnterEventArgs args)
+    {
+        if (args.interactableObject != null)
+        {
+            string itemTag = args.interactableObject.transform.tag;
+            if (!heldItemTags.Contains(itemTag))
+            {
+                heldItemTags.Add(itemTag);
+                
+            }
+        }
+    }
+
+    private void OnItemReleased(SelectExitEventArgs args)
+    {
+        if (args.interactableObject != null)
+        {
+            string itemTag = args.interactableObject.transform.tag;
+            heldItemTags.Remove(itemTag);
+            
+        }
+    }
+
+    public List<string> GetHeldItemTags()
+    {
+        return new List<string>(heldItemTags);
+    }
+}
