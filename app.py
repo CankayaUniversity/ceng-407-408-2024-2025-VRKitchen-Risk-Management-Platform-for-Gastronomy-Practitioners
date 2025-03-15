18ba3ec23f4e0dfdf0d25374386ed7af075ddab7
Işınsu Karagöz <118214808+isinsuu@users.noreply.github.com>
2025-03-15 15:44:34 +0300

    hotfix

--- app.py
+++ app.py
@@ -2,17 +2,13 @@ import os
 import json
 from dotenv import load_dotenv
 
-# Add OpenAI import  pip install openai==1.13.3 python-dotenv
-
 from openai import AzureOpenAI
 
 def main(): 
         
     try:
-        # Flag to show citations
         show_citations = False
 
-        # Get configuration settings 
         load_dotenv()
         azure_oai_endpoint = os.getenv("AZURE_OAI_ENDPOINT")
         azure_oai_key = os.getenv("AZURE_OAI_KEY")
@@ -21,16 +17,13 @@ def main():
         azure_search_key = os.getenv("AZURE_SEARCH_KEY")
         azure_search_index = os.getenv("AZURE_SEARCH_INDEX")
         
-        # Initialize the Azure OpenAI client
         client = AzureOpenAI(
             base_url=f"{azure_oai_endpoint}/openai/deployments/{azure_oai_deployment}/extensions",
             api_key=azure_oai_key,
             api_version="2023-09-01-preview")
 
-        # Get the prompt
         text = input('\nEnter a question:\n')
 
-        # Configure your data source
         extension_config = dict(dataSources = [  
                 { 
                     "type": "AzureCognitiveSearch", 
@@ -42,7 +35,6 @@ def main():
                 }]
             )
 
-        # Send request to Azure OpenAI model
         print("...Sending the following request to Azure OpenAI endpoint...")
         print("Request: " + text + "\n")
 
@@ -57,11 +49,9 @@ def main():
             extra_body = extension_config
         )
 
-        # Print response
         print("Response: " + response.choices[0].message.content + "\n")
 
         if (show_citations):
-            # Print citations
             print("Citations:")
             citations = response.choices[0].message.context["messages"][0]["content"]
             citation_json = json.loads(citations)
