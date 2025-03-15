a8bc5ab5eca28b933ab7f08b620c4830d37ea083
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2025-03-15 19:24:20 +0300

    teleportation for avatar tests

---
+++ VRKitchen-Unity/Assets/RouteManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class RouteManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class TeleportPoint
+    {
+        public Collider triggerZone;
+        public Transform teleportTarget;
+    }
+
+    public GameObject targetObject;
+    public TeleportPoint[] teleportPoints;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("a");
+        foreach (var point in teleportPoints)
+        {
+            if (other == point.triggerZone)
+            {
+                if (targetObject != null && point.teleportTarget != null)
+                {
+                    targetObject.transform.position = point.teleportTarget.position;
+                }
+            }
+        }
+    }
+}
---
+++ VRKitchen-Unity/Assets/Scenes/movementTest.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class movementTest : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0, moveZ).normalized;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
