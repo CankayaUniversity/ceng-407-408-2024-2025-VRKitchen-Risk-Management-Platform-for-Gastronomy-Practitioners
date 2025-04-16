64026f5bbfb6516c733b4cad7a9e708846290007
Zeynep Sıla Mert <zeysilamert@gmail.com>
2025-04-16 23:44:58 +0300

    oil pouring bug

--- VRKitchen-Unity/Assets/0VRKITCHEN/Scripts/Seasoning/OilPouring.cs
+++ VRKitchen-Unity/Assets/0VRKITCHEN/Scripts/Seasoning/OilPouring.cs
@@ -1,18 +1,17 @@
 using System.Collections;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class OilPouring : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private GameObject oilStream;     // The oil visual effect or stream object
-    [SerializeField] private Transform pourPoint; // Tip of the bottle where oil comes out
-    [SerializeField] private Transform bottleTransform; // The transform of the bottle itself
-    [SerializeField] private LayerMask dishLayer;      // Layer to detect dishes
+    [SerializeField] private GameObject oilStream;           // Yellow curved oil mesh
+    [SerializeField] private Transform pourPoint;            // Mesh origin (bottle mouth)
+    [SerializeField] private Transform bottleTransform;      // Bottle reference
+    [SerializeField] private LayerMask dishLayer;            // Raycast target layer
 
     [Header("Pouring Settings")]
-    [SerializeField] private float oilRate = 0.5f;      // Time between oil applications
-    [SerializeField] private float minTiltAngle = 60f;  // Minimum tilt angle to start pouring
+    [SerializeField] private float oilRate = 0.5f;
+    [SerializeField] private float minTiltAngle = 60f;
 
     private bool isPouring = false;
     private Coroutine pourCoroutine;
@@ -23,61 +22,49 @@ public class OilPouring : MonoBehaviour
         {
             oilStream.SetActive(false);
         }
-        else
-        {
-            Debug.LogError("Oil stream object not assigned!");
-        }
     }
 
     private void Update()
     {
-        float tiltAngle = Vector3.Angle(bottleTransform.up, Vector3.up);
-        //Debug.Log("Tilt angle: " + tiltAngle + " | Is Pouring: " + isPouring);
+        Vector3 pourDirection = -bottleTransform.up;
 
+        // 🔍 İlk olarak açıyı hesapla: Z ekseni etrafında ne kadar döneceğiz?
+        float angleZ = Vector3.SignedAngle(Vector3.forward, pourDirection, Vector3.forward);
+
+        // 🔁 Sadece Z ekseni etrafında döndür: X ve Y sabit kalır!
+        pourPoint.localRotation = Quaternion.AngleAxis(angleZ, Vector3.forward);
+
+        // Yağ dökme kontrolü
+        float tiltAngle = Vector3.Angle(bottleTransform.up, Vector3.up);
         if (tiltAngle > minTiltAngle)
         {
             if (!isPouring)
             {
-                //Debug.Log("Starting to pour oil");
-                StartPouring(-bottleTransform.up);
+                StartPouring(pourDirection);
             }
         }
         else
         {
             if (isPouring)
             {
-                //Debug.Log("Stopping oil pouring");
                 StopPouring();
             }
         }
-
     }
 
+    
     private void StartPouring(Vector3 direction)
     {
         isPouring = true;
-
-        if (oilStream != null)
-        {
-            oilStream.SetActive(true); // Show oil stream mesh or object
-        }
-
+        if (oilStream != null) oilStream.SetActive(true);
         pourCoroutine = StartCoroutine(PourOil(direction));
     }
 
     private void StopPouring()
     {
         isPouring = false;
-
-        if (oilStream != null)
-        {
-            oilStream.SetActive(false); // Hide oil stream
-        }
-
-        if (pourCoroutine != null)
-        {
-            StopCoroutine(pourCoroutine);
-        }
+        if (oilStream != null) oilStream.SetActive(false);
+        if (pourCoroutine != null) StopCoroutine(pourCoroutine);
     }
 
     private IEnumerator PourOil(Vector3 pourDirection)
