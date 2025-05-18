57d28ef00a8c6d6fd7ea27d4c23098a1efffc369
Ayşe Şimal Menekşe <94192946+simalmenekse@users.noreply.github.com>
2025-05-18 19:30:11 +0300

    Button hot-fix

--- VRKitchen-Unity/Assets/0VRKITCHEN/Scripts/KitchenTimer/TimerActivator.cs
+++ VRKitchen-Unity/Assets/0VRKITCHEN/Scripts/KitchenTimer/TimerActivator.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // ✅ Required for InputActionReference
+
 
 public class TimerActivator : MonoBehaviour
 {
     public TimerPanel timerPanel;
+    public InputActionReference toggleTimerAction;
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T) || toggleTimerAction.action.triggered)
         {
             timerPanel.TogglePanel();
         }
