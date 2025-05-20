ea71616021deeefd2869a0aefcf1e8ba04589f23
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2025-05-20 16:32:07 +0300

    controller to hand visuals

---
+++ VRKitchen-Unity/Assets/0VRKITCHEN/Scripts/XRHandController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public enum HandType
+{
+    Left, Right
+}
+
+public class XRHandController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
